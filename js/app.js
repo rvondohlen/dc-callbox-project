2af9825db5b23b57cd696c0bcf9e4bb9dd370bb7
rvondohlen <ryan@ryanvondohlen.com>
2014-05-12 22:39:06 -0400

    edits to styles and added edit mode button

--- js/app.js
+++ js/app.js
@@ -61,10 +61,18 @@ featureLayer.loadURL('callboxes.geojson')
 // adding a custom add button
 var mapContainer = document.getElementById('map');
 
+var editToggle =  document.createElement('h2')
+editToggle.id="edit-toggle";
+editToggle.innerHTML = '<a href="#">Enter editing mode &#x25B9;</a>';
+
+mapContainer.appendChild(editToggle);
+
 var addButton =  document.createElement('a')
 addButton.id="add";
 addButton.innerHTML = "+";
-mapContainer.appendChild(addButton);
+
+
+//mapContainer.appendChild(addButton);
 
 
 
